e3501cd8a878b966d537b9488c846a8e536ee4a7
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-11-06 21:32:46 +0700

    Latihan membuat static table view

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Yudi Setiawan on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
